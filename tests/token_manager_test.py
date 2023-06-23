c7ddf46657bbbe8bbe29f3397a0614457f7afae7
Matthias Beuerle <matthias.beuerle@meetiqm.com>
2023-06-23 09:21:12 +0200

    Use XDG_STATE_HOME variable for logs if it exists.

--- tests/token_manager_test.py
+++ tests/token_manager_test.py
@@ -19,6 +19,7 @@ import json
 import os
 from pathlib import Path
 import platform
+from unittest import mock
 
 from click.testing import CliRunner
 from mockito import ANY, unstub, when
@@ -57,15 +58,47 @@ def test_start_token_manager(credentials, config_dict, tokens_dict):
 @pytest.mark.skipif(platform.system().lower().startswith('win'), reason='daemonization does not work on windows')
 def test_daemonize_token_manager(config_dict):
     """
-    Tests that token manager refreshes tokens.
+    Tests that token manager daemonizes token management and logs progress.
     """
     when(daemon.DaemonContext).__enter__().thenReturn(None)  # pylint: disable=unnecessary-dunder-call
     when(daemon.DaemonContext).__exit__(ANY, ANY, ANY).thenReturn(None)
     when(token_manager).start_token_manager(1, ConfigFile(**config_dict)).thenReturn(None)
+    when(os).makedirs(f'{Path.home()}/.local/state', exist_ok=True).thenReturn(None)
+    with mock.patch('builtins.open', mock.mock_open()) as mock_log_file:
+        with mock.patch.dict(os.environ, {}):
+            token_manager.daemonize_token_manager(cycle=1, config=ConfigFile(**config_dict))
+    mock_log_file.assert_called_with(f'{Path.home()}/.local/state/token_manager.log', 'w', encoding='UTF-8')
+    unstub()
 
-    runner = CliRunner()
 
-    with runner.isolated_filesystem():
-        token_manager.daemonize_token_manager(cycle=1, config=ConfigFile(**config_dict))
-        assert os.path.isfile(f'{Path.home()}/.local/state/token_manager.log')
+@pytest.mark.skipif(platform.system().lower().startswith('win'), reason='daemonization does not work on windows')
+def test_daemonize_token_manager_uses_unix_recommended_log_dir(config_dict):
+    """
+    Tests that token manager daemonizes token management uses XDG_STATE_HOME variable for logs.
+    """
+    when(daemon.DaemonContext).__enter__().thenReturn(None)  # pylint: disable=unnecessary-dunder-call
+    when(daemon.DaemonContext).__exit__(ANY, ANY, ANY).thenReturn(None)
+    when(token_manager).start_token_manager(1, ConfigFile(**config_dict)).thenReturn(None)
+    mock_xdg_state_home = 'the/unix/way/to/store/stuff'
+    when(os).makedirs(mock_xdg_state_home, exist_ok=True).thenReturn(None)
+    with mock.patch('builtins.open', mock.mock_open()) as mock_log_file:
+        with mock.patch.dict(os.environ, {'XDG_STATE_HOME': mock_xdg_state_home}):
+            token_manager.daemonize_token_manager(cycle=1, config=ConfigFile(**config_dict))
+    mock_log_file.assert_called_with('the/unix/way/to/store/stuff/token_manager.log', 'w', encoding='UTF-8')
+    unstub()
+
+
+@pytest.mark.skipif(platform.system().lower().startswith('win'), reason='daemonization does not work on windows')
+def test_daemonize_token_manager_uses_set_log_dir(config_dict):
+    """
+    Tests that token manager daemonizes token management allows to use specific log directory.
+    """
+    when(daemon.DaemonContext).__enter__().thenReturn(None)  # pylint: disable=unnecessary-dunder-call
+    when(daemon.DaemonContext).__exit__(ANY, ANY, ANY).thenReturn(None)
+    when(token_manager).start_token_manager(1, ConfigFile(**config_dict)).thenReturn(None)
+    logfile = 'my/custom/file.log'
+    when(os).makedirs('my/custom', exist_ok=True).thenReturn(None)
+    with mock.patch('builtins.open', mock.mock_open()) as mock_log_file:
+        token_manager.daemonize_token_manager(cycle=1, config=ConfigFile(**config_dict), logfile=logfile)
+    mock_log_file.assert_called_with(logfile, 'w', encoding='UTF-8')
     unstub()
